cf5611f26a451e55df6f2a1fc43a2c104ae58184
parnavithalkar <parnavithalkar04@gmail.com>
2025-02-22 14:26:47 +0530

    2 commit

--- flipkart/clothing/models.py
+++ flipkart/clothing/models.py
@@ -23,5 +23,5 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='product_img/')
 
-def __str__(self):
+    def __str__(self):
         return self.name 
